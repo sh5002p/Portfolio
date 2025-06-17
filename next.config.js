fd34ed4159095319c7bb490c03ee9a587e5ba210
sh5002p <yanisrechal1@gmail.com>
2025-06-17 18:18:03 +0200

    ooui

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',  // Cela indique que tu veux exporter un site statique
-    distDir: 'out',    // Cela définit le dossier de sortie pour l'export
+    basePath: '/Portfolio',  // Le chemin de ton site sur GitHub Pages
+    assetPrefix: '/Portfolio', // Cela garantit que les assets sont chargés correctement
+    output: 'export',
+    distDir: 'out',
   };
   
   module.exports = nextConfig;
