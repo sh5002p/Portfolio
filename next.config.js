503d75448e51d07ea7a32fea92f3d634dbfde5ae
sh5002p <yanisrechal1@gmail.com>
2025-06-17 18:13:05 +0200

    non

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export', 
-    distDir: 'out',   
+    output: 'export',  // Cela indique que tu veux exporter un site statique
+    distDir: 'out',    // Cela définit le dossier de sortie pour l'export
   };
   
   module.exports = nextConfig;
